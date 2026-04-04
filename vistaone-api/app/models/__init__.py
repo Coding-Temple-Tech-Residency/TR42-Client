f7a8a6a6ec1fcbc8d1aa04810e04c8c71eda362c
shaney hoyohoy <shakalili86@gmail.com>
2026-04-04 02:35:55 -1000

    fixed folder structure:

--- vistaone-api/app/models/__init__.py
+++ vistaone-api/app/models/__init__.py
@@ -1,10 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 
+
 class Base(DeclarativeBase):
     pass
 
 db = SQLAlchemy(model_class=Base)
 
-# Import models so SQLAlchemy registers them
+
 from .user import User
+from .vendor import Vendor
+from .workorder import WorkOrder
+
+
+
+
