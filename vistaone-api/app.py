2dcd09f8917f13e4714f7525b9e7d67b3d04d725
Dhanushka <dhanushkamcr@gmail.com>
2026-04-14 18:44:20 -0500

    Update well model

--- vistaone-api/app.py
+++ vistaone-api/app.py
@@ -1,7 +1,7 @@
 from app import create_app
 from app.extensions import db
 
-app = create_app('ProductionConfig')
+app = create_app("ProductionConfig")
 
 # with app.app_context():
 #      #db.drop_all()
