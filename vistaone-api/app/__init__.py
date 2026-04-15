95309bde8518ac67fad2809c5f9f24caa9136215
Dhanushka <dhanushkamcr@gmail.com>
2026-04-15 02:27:43 -0500

    Use flask marshmallow instead of marshmallow in well schema

--- vistaone-api/app/__init__.py
+++ vistaone-api/app/__init__.py
@@ -2,78 +2,63 @@ from flask import Flask, app, jsonify
 from app.extensions import ma, limiter, db
 from app.blueprints.controller import users_bp
 from app.blueprints.controller import workorder_bp
+from app.blueprints.controller import well_bp
 from app.utils.logging_util import logging_setup
 from flask_swagger_ui import get_swaggerui_blueprint
 from dotenv import load_dotenv
 from flask_cors import CORS
 
-
 # Load .env file
 load_dotenv()
 
-SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
-API_URL = '/static/swagger.yaml'  # Our API URL (can of course be a local resource)
+SWAGGER_URL = "/api/docs"  # URL for exposing Swagger UI (without trailing '/')
+API_URL = "/static/swagger.yaml"  # Our API URL (can of course be a local resource)
 
 swaggerui_blueprint = get_swaggerui_blueprint(
-    SWAGGER_URL,
-    API_URL,
-    config={
-        'app_name': "Client Web Dashboard"
-    }
+    SWAGGER_URL, API_URL, config={"app_name": "Client Web Dashboard"}
 )
 
 
-def create_app(config_name):
+def create_app(config_name="ProductionConfig"):
     app = Flask(__name__)
-    app.config.from_object(f'config.{config_name}')
+    app.config.from_object(f"config.{config_name}")
 
-    app.url_map.strict_slashes
-    
+    app.url_map.strict_slashes = False
+
     # Initialize extensions
     ma.init_app(app)
     db.init_app(app)
     limiter.init_app(app)
 
-    logging_setup() 
+    logging_setup()
 
     # Register blueprints
-    app.register_blueprint(users_bp, url_prefix='/users')
-    app.register_blueprint(workorder_bp, url_prefix='/workorders')
+    app.register_blueprint(users_bp, url_prefix="/users")
+    app.register_blueprint(workorder_bp, url_prefix="/workorders")
+    app.register_blueprint(well_bp, url_prefix="/wells")
     app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
-    
+
     CORS(
         app,
-        origins = ["http://localhost:5173"],
-        allow_headers = ["Content-Type", "Authorization"],
-        methods =["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+        origins=["http://localhost:5173"],
+        allow_headers=["Content-Type", "Authorization"],
+        methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     )
 
     @app.errorhandler(429)
     def handle_rate_limit(_):
-        return jsonify({'message': 'Too many requests. Please try again later.'}), 429
-    
+        return jsonify({"message": "Too many requests. Please try again later."}), 429
 
     @app.errorhandler(401)
     def unauthorized_error(e):
-        return jsonify({
-        "status": "error",
-        "message": "Unauthorized access"
-    }), 401
-
+        return jsonify({"status": "error", "message": "Unauthorized access"}), 401
 
     @app.errorhandler(404)
     def not_found(e):
-        return jsonify({
-        "status": "error",
-        "message": "Resource not found"
-    }), 404
-
+        return jsonify({"status": "error", "message": "Resource not found"}), 404
 
     @app.errorhandler(500)
     def server_error(e):
-        return jsonify({
-        "status": "error",
-        "message": "Internal server error"
-    }), 500
-    
+        return jsonify({"status": "error", "message": "Internal server error"}), 500
+
     return app
--- vistaone-api/app/blueprints/schema/well_schema.py
+++ vistaone-api/app/blueprints/schema/well_schema.py
@@ -1,8 +1,13 @@
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from app.extensions import ma
 from app.models.wells import Well
 
-class WellSchema(SQLAlchemyAutoSchema):
+
+class WellSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Well
         load_instance = True
         include_fk = True
+
+
+well_schema = WellSchema()
+wells_schema = WellSchema(many=True)
