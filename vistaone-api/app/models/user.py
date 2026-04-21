fbfe8050cf795b0ca2b8c1c677f92f603bafee3b
Dhanushka <dhanushkamcr@gmail.com>
2026-04-21 11:13:16 -0500

    Update nullable true fields in user model

--- vistaone-api/app/models/user.py
+++ vistaone-api/app/models/user.py
@@ -20,13 +20,13 @@ class User(db.Model, AuditMixin):
     user_type: Mapped[UserType] = mapped_column(db.Enum(UserType), nullable=False)
     status: Mapped[UserStatus] = mapped_column(db.Enum(UserStatus), nullable=False)
     first_name: Mapped[str] = mapped_column(db.String(80), nullable=False)
-    middle_name: Mapped[str] = mapped_column(db.String(80))
+    middle_name: Mapped[str] = mapped_column(db.String(80), nullable=True)
     last_name: Mapped[str] = mapped_column(db.String(80), nullable=False)
-    profile_photo_url: Mapped[str] = mapped_column(db.String(255))
-    date_of_birth: Mapped[date] = mapped_column(Date)
-    ssn_last_four: Mapped[str] = mapped_column(db.CHAR(4))
+    profile_photo_url: Mapped[str] = mapped_column(db.String(255), nullable=True)
+    date_of_birth: Mapped[date] = mapped_column(Date, nullable=True)
+    ssn_last_four: Mapped[str] = mapped_column(db.CHAR(4), nullable=True)
     contact_number: Mapped[str] = mapped_column(db.String(30), nullable=False)
-    alternate_number: Mapped[str] = mapped_column(db.String(30))
+    alternate_number: Mapped[str] = mapped_column(db.String(30), nullable=True)
 
     client_id: Mapped[str] = mapped_column(
         db.String(36), db.ForeignKey("client.id"), nullable=False
