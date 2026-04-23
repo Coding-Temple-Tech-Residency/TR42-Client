47ccf67366bf3d937c0aa0f575487e218431183f
Dhanushka <dhanushkamcr@gmail.com>
2026-04-23 02:25:14 -0500

    Add admin user schema

--- vistaone-api/app/blueprints/schema/register_client_schema.py
+++ vistaone-api/app/blueprints/schema/register_client_schema.py
@@ -3,6 +3,15 @@ from marshmallow import fields
 from app.blueprints.schema.address_schema import AddressSchema
 
 
+class AdminUserSchema(ma.Schema):
+    username = fields.String(required=True)
+    email = fields.Email(required=True)
+    password = fields.String(required=True, load_only=True)
+    first_name = fields.String(required=True)
+    last_name = fields.String(required=True)
+    contact_number = fields.String(required=True)
+
+
 class RegisterClientSchema(ma.Schema):
     client_name = fields.String(required=True)
     client_code = fields.String(required=True)
@@ -11,6 +20,7 @@ class RegisterClientSchema(ma.Schema):
     company_contact_number = fields.String(required=True)
     company_web_address = fields.String(load_default=None)
     address = fields.Nested(AddressSchema, required=True)
+    admin_user = fields.Nested(AdminUserSchema, required=True)
 
 
 register_client_schema = RegisterClientSchema()
