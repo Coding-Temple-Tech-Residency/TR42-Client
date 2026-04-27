152a2afe9db795b0fe151b6ae40a3f8b3da98f58
Dhanushka <dhanushkamcr@gmail.com>
2026-04-27 01:03:31 -0500

    Inherit audit fields to msa

--- vistaone-api/app/models/msa.py
+++ vistaone-api/app/models/msa.py
@@ -2,9 +2,10 @@ from sqlalchemy.orm import mapped_column, relationship
 from sqlalchemy.sql import func
 import uuid
 from app.extensions import db
+from app.models.audit_mixin import AuditMixin
 
 
-class Msa(db.Model):
+class Msa(db.Model, AuditMixin):
     __tablename__ = "msa"
 
     id = mapped_column(
@@ -17,10 +18,6 @@ class Msa(db.Model):
     status = mapped_column(db.String(15), nullable=False, default="active")
     uploaded_by = mapped_column(db.String(100), nullable=True)
     file_name = mapped_column(db.String(255), nullable=True)
-    created_by = mapped_column(db.String(100))
-    created_date = mapped_column(db.DateTime, server_default=func.now())
-    last_modified_by = mapped_column(db.String(100))
-    last_modified_date = mapped_column(db.DateTime)
 
     ## Relationships
     vendor = relationship("Vendor")
